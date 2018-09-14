53d253810fca73c3cb457e252323e35978627f4e
Arthur Jochems <detuur@gmail.com>
2018-09-15 00:33:37 +0200

    Rewrote boss-key, updated README
    Largely rewritten, now much much more performant and will instantly minimise your window.

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ just one.
 ## boss-key.lua
 Started out as a gist, and win32 only. Only after a while I noticed 
 [@zaza42](https://github.com/zaza42) had forked it and added the Linux
-functionality I had asked for.
+functionality I had asked for. I've taken the opportunity to rework it since
+the performance was terrible (taking upwards of a second to minimise after
+keypress). I've largely rewritten it and eliminated the lag. Future possible
+features I've considered are things like darkening the screen so that
+taskbar miniatures don't reveal anything either.
--- boss-key.lua
+++ boss-key.lua
@@ -1,4 +1,5 @@
 -- AUTHORS: detuur, zaza42
+-- License: MIT
 -- link: https://github.com/detuur/mpv-scripts
 
 -- This script minimises and pauses the window when
@@ -25,33 +26,71 @@ utils = require 'mp.utils'
 function boss_key()
 	mp.set_property_native("pause", true)
 	if platform == 'windows' then
-	    minimize_win32()
+	    mp.command([[run cmd /c echo m > \\.\pipe\mpv-boss-key-]]..utils.getpid())
 	elseif platform == 'linux' then
 	    utils.subprocess({ args = {'xdotool', 'getactivewindow', 'windowminimize'} })
 	end
 end
 
--- This function is still mind-boggingly slow. It's because we need to compile
--- the function call to the win32 API every time we run it. This takes about
--- a second.
--- TODO: cut down on this downtime
-function minimize_win32()
-	local res = utils.subprocess({
-		args = {'powershell', '-NoProfile', '-Command', [[& {
-            # Get mpv's PID
-            $bosspid = (gwmi win32_process | ? processid -eq $pid).parentprocessid
+-- Windows is a princess (a very ugly one at that) and needs an external process
+-- to minimize mpv, with which we communicate through named pipes. I spent an
+-- entire day shouting at my screen to arrive at the code below. This went
+-- through several complete rewrites as every iteration broke in subtly
+-- different ways. Did you know that Powershell and CMD's echo commands behave
+-- slightly differently, one refusing to properly write to some streams where
+-- the other doesn't? Did you know Powershell's Events and any kind of async
+-- is absolute DOG SHIT which will happily chooch in an interactive session only
+-- to shit in your face when put inside a script? Honestly I could write several
+-- kb about the bullshit I've encountered but it's past midnight and I just want
+-- to watch my weeb shit. Fuck you microsoft.
+if platform == 'windows' then
+    utils.subprocess_detached({
+      args = {'powershell', '-NoProfile', '-Command', [[&{
+$bosspid = ]]..utils.getpid()..[[
 
-            # Set function signature
-            $signature='[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+# Create the named pipe
+$pipename = -join('mpv-boss-key-',$bosspid)
+$fullpipename = -join("\\.\pipe\", $pipename)
 
-            # Call Add-Type to compile code 
-            $showWindowAsync = Add-Type -memberDefinition $signature -name "Win32ShowWindowAsync" -namespace Win32Functions -passThru 
+# This will run in a separate thread
+$minimizeloop = {
+    param($pipename, $bosspid)
+    # Create the named pipe and buffer
+    $pipe = new-object System.IO.Pipes.NamedPipeServerStream($pipename)
 
-            # Minimize mpv 
-            $showWindowAsync::ShowWindowAsync((Get-Process -id $bosspid).MainWindowHandle, 2)
-        }]]},
-		cancellable = false,
-	})
+    # Compile the Win32 API function call
+    $signature='[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+    $showWindowAsync = Add-Type -memberDefinition $signature -name "Win32ShowWindowAsync" -namespace Win32Functions -passThru
+
+    # The core loop
+    while($true) {
+        $pipe.WaitForConnection()
+        if ($pipe.ReadByte() -ne 109) {
+            break 
+        }
+        $pipe.Disconnect()
+        $showWindowAsync::ShowWindowAsync((Get-Process -id $bosspid).MainWindowHandle, 2)
+    }
+    $pipe.Dispose()
+}
+
+# Exiting this monstrosity (THANKS POWERSHELL FOR BROKEN ASYNC) is surprisingly
+# cumbersome. It took literal hours to find something that didn't spontaneously
+# combust. Fuck windows.
+$bossproc = Get-Process -pid $bosspid -ErrorAction SilentlyContinue
+$exitsequence = {
+    &{echo q > $fullpipename} 2> $null
+    [Environment]::Exit(0)
+}
+if ((-Not $bossproc) -or $bossproc.HasExited) { $exitsequence.Invoke() }
+
+# Begin watching for events until boss closes
+Start-Job -ScriptBlock $minimizeloop -Name "mpvminloop" -ArgumentList $pipename,$bosspid
+while($true) {
+    Start-Sleep 1
+    if ($bossproc.HasExited) { $exitsequence.Invoke() }
+}
+} > R:\pipedebug\log]]}})
 end
 
 mp.add_key_binding('b', 'boss-key', boss_key)
